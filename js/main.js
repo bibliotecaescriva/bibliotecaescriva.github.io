07974d7f36c68b965d6d302c81aa4808959fd310
GersonDantas <gerson.accari@hotmail.com>
2019-10-15 18:56:21 -0300

    gerson

--- js/main.js
+++ js/main.js
@@ -35,37 +35,11 @@ $(function() {
           $('#aside1').hide();
           $('a.toggleSidebar span.fas').removeClass('fa-angle-left');
           $('a.toggleSidebar span.fas').addClass('fa-angle-right');
-        }        
+        }
       });
     } else {
       //aside começa escondido
       $('#aside1').hide();      
-      $(function(){
-        $("a.toggleSidebar").hover(
-          function(){
-                //Ao posicionar o cursor sobre a div
-                $('#aside1').css('box-shadow', '5px 5px 5px 5px  rgba(0,0,0,0.2)');
-              },
-              function(){
-                //Ao remover o cursor da div
-                $('#aside1').css('box-shadow', '5px 5px 5px 5px  rgba(0,0,0,0');
-              }
-          );
-      }); 
-      //animação aside
-      $('#aside1').css('width','215px');
-      $(document).off('click', 'a.toggleSidebar');
-      $(document).on('click', 'a.toggleSidebar', function() {
-        if ($('#aside1').css('display') == 'none') {
-          $('#aside1').show();
-          $('a.toggleSidebar span.fas').removeClass('fa-angle-right');
-          $('a.toggleSidebar span.fas').addClass('fa-angle-left');
-        } else {
-          $('#aside1').hide();
-          $('a.toggleSidebar span.fas').removeClass('fa-angle-left');
-          $('a.toggleSidebar span.fas').addClass('fa-angle-right');
-        }
-      });
     }
   }).resize();
 });
